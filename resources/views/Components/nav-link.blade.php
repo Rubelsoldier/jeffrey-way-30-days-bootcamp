d36c506260d279670b98a40ec9312755a262eff0
Rubel Ahmed <professional.r.rana@gmail.com>
2025-06-08 11:32:52 +0600

    till-day-5 ( with a test of type props )

---
+++ resources/views/Components/nav-link.blade.php
@@ -0,0 +1,19 @@
+@props(['active'=>false,'type'=>'button'])
+
+@if ($type === 'link')
+    <a
+        class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+        aria-current="{{ $active ? 'page' : 'false' }}"
+        {{ $attributes }}
+    >
+        {{ $slot }}
+    </a>
+@else
+    <button
+        type="{{ $type }}"
+        class="bg-gray-900 text-red-800 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-bold"
+        {{ $attributes }}
+    >
+        {{ $slot }}
+    </button>
+@endif
---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    
+    <x-slot:heading>
+        about page
+    </x-slot:heading>
+    <h1>I am in about page</h1>    
+</x-layout>
