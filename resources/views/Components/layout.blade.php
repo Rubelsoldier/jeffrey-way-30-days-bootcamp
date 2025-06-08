9d62e89d35214b42a40edf28ad8b177652351901
Rubel Ahmed <professional.r.rana@gmail.com>
2025-06-08 14:25:56 +0600

    class-5-test

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -28,11 +28,10 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               {{-- <a href="/" class="{{ request()->path() === '/' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a> --}}
-              <x-nav-link type="link" href="/" :active="request()->path() === '/'">Home</x-nav-link>
-              <x-nav-link type="link" href="/about" :active="request()->is('about')">About</x-nav-link>
-              <x-nav-link type="link" href="/contact" :active="request()->is('contact')">Contact</x-nav-link>              
+              <x-nav-link href="/" :active="request()->path() === '/'">Home</x-nav-link>
+              <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+              <x-nav-link type="button" :active="request()->is('contact')">Contact</x-nav-link>              
             </div>
-              <x-nav-link type="button">Meau</x-nav-link>
           </div>
         </div>
         <div class="hidden md:block">
