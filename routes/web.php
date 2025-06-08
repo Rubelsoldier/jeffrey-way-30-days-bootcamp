2ceb7764f8a4a940f3cdd025ca3d57a149d6c481
Rubel Ahmed <nus01ta3@gmail.com>
2025-06-08 22:30:47 +0600

    Autoloading, Namespaces, and Models
    day-7

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs',[
-        'jobs' => [
-            ['id' => 1, 'title' => 'Software Engineer', 'salary' => 70000],
-            ['id' => 2, 'title' => 'Data Scientist', 'salary' => 80000],
-            ['id' => 3, 'title' => 'Web Developer', 'salary' => 60000],
-        ]
+    return view('jobs', [
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-            ['id'=>1 , 'title' => 'Software Engineer', 'salary' => 70000],
-            ['id'=>2 , 'title' => 'Data Scientist', 'salary' => 80000],
-            ['id'=>3 , 'title' => 'Web Developer', 'salary' => 60000],
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-    
+    $job = Job::find($id);
     return view('job', [
         'job' => $job
     ]);
