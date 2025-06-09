4cde262b766a2644ca1efe44dfd553c6c9f85fe3
Rubel Ahmed <nus01ta3@gmail.com>
2025-06-09 07:33:44 +0600

    Meet Eloquent
    day 9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,31 +1,10 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array {
-        return [
-            ['id' => 1, 'title' => 'Software Engineer', 'salary' => 70000],
-            ['id' => 2, 'title' => 'Data Scientist', 'salary' => 80000],
-            ['id' => 3, 'title' => 'Web Developer', 'salary' => 60000],
-        ];
-    }
-
-    public static function find($id): ?array {
-        $jobs = self::all();
-        foreach ($jobs as $job) {
-            if ($job['id'] == $id) {
-                return $job;
-            }
-        }
-
-        // condition for aborting
-        if ($id < 1 || $id > count($jobs)) {
-            abort(404, 'Job not found');
-        }
-
-        return null;
-    }
+class Job extends Model {    
+    protected $table = 'job_listings'; 
 }
 
 ?>
